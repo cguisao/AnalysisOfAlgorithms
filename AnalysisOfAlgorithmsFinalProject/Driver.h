694c54ea2ccc62ed63b4748d359efaca776e23eb
Carlos Guisao <cguisao@fau.edu>
2016-04-07 00:19:40 -0400

    Added a driver to be able to create arrays using both classes and the main can be cleaner

---
+++ AnalysisOfAlgorithmsFinalProject/Driver.h
@@ -0,0 +1,33 @@
+// ***********************************************************************
+//
+// (c) Copyright 2014 Carlos Guisao 
+//
+//  All rights reserved.
+//
+//  cguisao@fau.edu
+//
+// ***********************************************************************
+#include <iostream>
+#include "Dynamic.h"
+#include "Recursive.h"
+
+
+using namespace std;
+
+class Driver
+{
+public:
+	Driver();
+	~Driver();
+
+private:
+
+};
+
+Driver::Driver()
+{
+}
+
+Driver::~Driver()
+{
+}
